087c3e1eb2f38f4fbd93fd6b09f0156ccd0e6ee4
skyGom <dprha4789@gmail.com>
2024-09-30 20:47:41 +0900

    24.09.30 ORM Practice

--- tweets/admin.py
+++ tweets/admin.py
@@ -5,8 +5,12 @@ from .models import Tweet, Like
 class TweetAdmin(admin.ModelAdmin):
     """Tweet Admin Definition"""
     
-    list_display = ("user", "payload",)
+    list_display = ("user", "payload", "likes_count", "created_at",)
     list_filter = ("user",)
+    
+    def likes_count(self, tweet):
+        return tweet.likes.count()
+    likes_count.short_description = "Like Count"
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
--- tweets/models.py
+++ tweets/models.py
@@ -4,15 +4,16 @@ from common.models import CommonModel
 class Tweet(CommonModel):
     """Tweet Model Definition"""
     payload = models.TextField(max_length=180,)
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE,)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="tweets",)
+    like = models.ManyToManyField("tweets.Like", related_name="tweets", blank=True,)
     
     def __str__(self):
         return f"{self.user} says: {self.payload}"
     
 class Like(CommonModel):
     """Like Model Definition"""
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE,)
-    tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE,)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="likes",)
+    tweet = models.ForeignKey("tweets.Tweet", on_delete=models.CASCADE, related_name="likes",)
     
     def __str__(self):
         return f"{self.user} likes {self.tweet}"
